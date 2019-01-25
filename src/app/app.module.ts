b37512f248936d220428a66c039b27b0745874ac
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2019-01-25 13:39:54 -0600

    Worked on navigation bar and its getting displayed properly in dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 import {MatInputModule} from '@angular/material/input';
 
 
@@ -15,12 +16,14 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     LoginComponent,
     DashboardComponent
+    
   ],
   imports: [
     BrowserModule,
@@ -31,6 +34,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MatFormFieldModule,
     MatCardModule,
     MatButtonModule,
+    MatMenuModule,
     MatInputModule
     
   ],
