1282774eb41ef80c65a43f9c8d62ee0af023d004
ANURAG KUMAR <s531372@nwmissouri.edu>
2019-01-25 08:22:43 -0600

    Worked on Forget Passsword Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
-
+import {ForgetpwdComponent } from './forgetpwd/forgetpwd.component'
 
 const routes: Routes = [
   { path: 'login' , component: LoginComponent },
   { path: 'dashboard' , component: DashboardComponent },
   { path: 'register' , component : RegisterComponent },
+  {path: 'forgetpwd',component: ForgetpwdComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
+import { ForgetpwdComponent } from './forgetpwd/forgetpwd.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { RegisterComponent } from './register/register.component';
     NavBarComponent,
     LoginComponent,
     DashboardComponent,
-    RegisterComponent
+    RegisterComponent,
+    ForgetpwdComponent
   ],
   imports: [
     BrowserModule,
