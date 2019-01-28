97d867c1d15c3a4b520ea2e96e70c405c3343836
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2019-01-28 13:49:24 -0600

    Created snack bar for showing feedback messages in user interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatDialogModule } from '@angular/material/dialog';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 // import { PasswordComponent } from './password/password.component';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -17,9 +22,9 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
-
 import { CodewordsetComponent } from './codewordset/codewordset.component';
 import { PasswordComponent } from './password/password.component';
+import { AddUserComponent, AddUserSnackBarComponent } from './add-user/add-user.component';
 
 
 
@@ -33,24 +38,36 @@ import { PasswordComponent } from './password/password.component';
     
     CodewordsetComponent,
     PasswordComponent,
+    AddUserComponent,
+    AddUserSnackBarComponent
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
     MatToolbarModule,
-    MatFormFieldModule,
-    MatCardModule,
     MatButtonModule,
     MatInputModule,
+    MatCardModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
     MatDialogModule,
-    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,    
+    ReactiveFormsModule
 
 
   ],
   providers: [],
+  entryComponents: [
+    AddUserComponent,
+    AddUserSnackBarComponent
+        
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
