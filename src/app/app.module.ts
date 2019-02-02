5015f1e57f2b6f4ff33208fa41f68444f8166579
NaveenKumar Nuggu <naveennuggu.acs@gmail.com>
2019-02-01 19:43:24 -0600

    Modifying the registration page UI using Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 
+
 // import { PasswordComponent } from './password/password.component';
 
 import { AppRoutingModule } from './app-routing.module';
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -7,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  hide = true;
+  
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
   constructor() { }
 
   ngOnInit() {
