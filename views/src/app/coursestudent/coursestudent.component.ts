d5b08061c16add345a435149ce2cfed43954895c
ANURAG KUMAR <s531372@nwmissouri.edu>
2019-02-08 09:47:04 -0600

    coursestudent page and funtionality added

--- views/src/app/coursestudent/coursestudent.component.ts
+++ views/src/app/coursestudent/coursestudent.component.ts
@@ -1,31 +1,60 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatPaginator, MatTableDataSource} from '@angular/material';
-
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ViewChild } from '@angular/core';
 
 
 export interface PeriodicElement {
- 
   email: string;
+  name: string;
   codeword: string;
-  acknowledge: boolean;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {email: 's531519@nwmissouri.edu',  codeword: 'Africa', acknowledge: true},
-  {email: 's530742@nwmissouri.edu',  codeword: 'Almond', acknowledge: false},
-  {email: 's531495@nwmissouri.edu',  codeword: 'Anger', acknowledge: true},
-  {email: 's531367@nwmissouri.edu',  codeword: 'America', acknowledge: true},
-  {email: 's531496@nwmissouri.edu',  codeword: 'Bravo', acknowledge: false},
-  {email: 's531486@nwmissouri.edu',  codeword: 'Bank', acknowledge: true},
-  {email: 's531369@nwmissouri.edu',  codeword: 'Bounce', acknowledge: true},
-  {email: 's531499@nwmissouri.edu',  codeword: 'Bigger', acknowledge: false},
-  {email: 's531500@nwmissouri.edu',  codeword: 'Computer', acknowledge: true},
-  {email: 's531372@nwmissouri.edu',  codeword: 'Cancer', acknowledge: true},
-  {email: 's530473@nwmissouri.edu',  codeword: 'Chair', acknowledge: false},
-  {email: 's531439@nwmissouri.edu',  codeword: 'Center', acknowledge: true},
+const ELEMENT_DATA: PeriodicElement[] = [{
+  email: "sspennock0@addthis.com",
+  name: "Sonnie Spennock",
+  codeword: "Myanmar"
+}, {
+  email: "lhacking1@msn.com",
+  name: "Letisha Hacking",
+  codeword: "China"
+}, {
+  email: "dminet2@google.fr",
+  name: "Dorise Minet",
+  codeword: "Poland"
+}, {
+  email: "ssolomonides3@ucoz.ru",
+  name: "Selestina Solomonides",
+  codeword: "Brazil"
+}, {
+  email: "ahenzley4@google.com.br",
+  name: "Anthiathia Henzley",
+  codeword: "Portugal"
+}, {
+  email: "mkleinplatz5@w3.org",
+  name: "Marjie Kleinplatz",
+  codeword: "Philippines"
+}, {
+  email: "ewilmington6@sogou.com",
+  name: "Erika Wilmington",
+  codeword: "Thailand"
+}, {
+  email: "alafflin7@miitbeian.gov.cn",
+  name: "Angie Lafflin",
+  codeword: "Indonesia"
+}, {
+  email: "rwharton8@apache.org",
+  name: "Randall Wharton",
+  codeword: "Ukraine"
+}, {
+  email: "bodreain9@scribd.com",
+  name: "Brande O'Dreain",
+  codeword: "China"
+}]
 
-];
 
 @Component({
   selector: 'app-coursestudent',
@@ -34,13 +63,26 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class CoursestudentComponent implements OnInit {
 
-  displayedColumns: string[] = ['email', 'codeword', 'acknowledge'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['email', 'name', 'codeword'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
+  @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
+  constructor(public dialog: MatDialog, private router: Router) { }
+
   ngOnInit() {
+    this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue;
+  }
+
+ 
+  rowClicked(row: any): void {
+    console.log(row);
+    this.router.navigate(['/dashboard'])
+  }  
 }
