bc8f180f4c3c544c6435808ffab04a14c7a0c879
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2019-02-11 19:01:33 -0600

    Worked on Error message and validation part for Login component

--- views/src/app/login/login.component.ts
+++ views/src/app/login/login.component.ts
@@ -9,7 +9,7 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
+
   errFlag = false;
 
   constructor(private router: Router, private userService: UserService) { }
@@ -17,37 +17,29 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLogin(data){
-    if(data.valid){
+  onLogin(data) {
+    if (data.valid) {
       console.log(data.value);
       this.userService
-      .validateEmail(data.value)
-      .subscribe((res : any) => {
-        console.log(res)
-        if (res && res.message) {
-          this.userService
-            .signin(data.value)
-            .subscribe((res : any) => {
-              localStorage.setItem('token', res.token)
-              localStorage.setItem('status', res.isInstructor)
-              this.router.navigate(['/dashboard'])
-    },
-    err => {
-      console.log(err)
-        alert("Invalid credentials");
-     
-    })
-    } else {
-       alert('Invalid Email')
-     
-    }
-  },
-  err => {
-    console.log(err)
-    // alert("Invalid credentials");
-  })
-    
-  }
+        .validateEmail(data.value)
+        .subscribe((res: any) => {
+          console.log(res)
+          if (res && res.message) {
+            this.userService
+              .signin(data.value)
+              .subscribe((res: any) => {
+                localStorage.setItem('token', res.token)
+                localStorage.setItem('status', res.isInstructor)
+                this.router.navigate(['/dashboard'])
+              },
+            )
 
-}
+          } else {
+            this.errFlag = true;
+            data.resetForm();
+          }
+        },
+      )
+    }
+  }
 }
