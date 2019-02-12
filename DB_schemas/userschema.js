794e504b6b86d4ff5775522c3cf37660c4ead8e3
NaveenKumar Nuggu <naveennuggu.acs@gmail.com>
2019-02-11 20:18:27 -0600

    Unique email validater

--- DB_schemas/userschema.js
+++ DB_schemas/userschema.js
@@ -1,10 +1,11 @@
 var mongoose = require("mongoose");
+var uniqueValidator = require('mongoose-unique-validator');
 
 var userschema = new mongoose.Schema({
-    emailID:String,
+    emailID: { type: String,unique: true, required: true },
     passwd:String,
     isInstructor:Boolean
 
 });
-
+ userschema.plugin(uniqueValidator);
 module.exports.userschema=userschema;
