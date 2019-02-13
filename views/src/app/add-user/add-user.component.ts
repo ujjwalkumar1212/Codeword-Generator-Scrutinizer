876e0b16430334472579fe774ae708954761e8b9
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2019-02-12 18:26:37 -0600

    Worked on adding codewordset to database and fetching the same.

--- views/src/app/add-user/add-user.component.ts
+++ views/src/app/add-user/add-user.component.ts
@@ -3,6 +3,8 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CodewordsetService } from 'src/app/services/codewordset.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-user',
@@ -13,10 +15,13 @@ export class AddUserComponent implements OnInit {
   
   errFlag = false;
   adduser = '';
+  tcodeWordSetData: any
+  isFileUploaded = false;
+  codeWordCount = 0;
 
-  constructor(public dialogRef: MatDialogRef<AddUserComponent>,
+  constructor(public dialog: MatDialog, public dialogRef: MatDialogRef<AddUserComponent>,
     @Inject(MAT_DIALOG_DATA) public data,
-    public snackBar: MatSnackBar) { 
+    public snackBar: MatSnackBar, private codeWordSetService: CodewordsetService) { 
       this.adduser = { ...data};
     }
 
@@ -24,14 +29,52 @@ export class AddUserComponent implements OnInit {
   ngOnInit() {
   }
 
+  // Function to preview the count of uploaded file
+  previewFiles(event: any) {
+    let files = event.target.files[0];
+    let data = new FormData();
+    data.append('file', files)
+    this.codeWordSetService
+      .previewFiles(data)
+      .subscribe((res: any) => {
+        console.log(res)
+        this.isFileUploaded = true;
+        this.codeWordCount = res.count;
+        this.tcodeWordSetData = res.data;
+      },
+      err => {
+        console.log(err)
+      })
+  }
+
   save(data) {
     if (data.valid) {
       console.log(data.value);
       // this.router.navigate(['/user'])
+      let sendData = {
+        CodeWordSetName: data.value.name
+      }
+      let sendData2 = {
+        CodeWordSetName: data.value.name,
+        Codewords: this.tcodeWordSetData
+      }
+      this.codeWordSetService
+        .saveCodewordSet(sendData)
+        .subscribe((res: any) => {
+          this.codeWordSetService
+            .saveCodewords(sendData2)
+            .subscribe((res: any) => {
       this.snackBar.openFromComponent(AddUserSnackBarComponent, {
       duration: 750,
     });
-      this.dialogRef.close()
+      this.dialogRef.close()},
+      err => {
+        console.log(err)
+      })
+  },
+  err => {
+    console.log(err)
+  })
     } else {
       this.errFlag = true;
       data.reset
