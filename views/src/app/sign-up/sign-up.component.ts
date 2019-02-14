c1a29e18aba3a5907e9a2bf1227064de42604d27
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2019-02-13 21:06:00 -0600

    Removed unwanted codes

--- views/src/app/sign-up/sign-up.component.ts
+++ views/src/app/sign-up/sign-up.component.ts
@@ -11,83 +11,37 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class SignUpComponent implements OnInit {
 
-  // hide = true;
-  
-  // email = new FormControl('', [Validators.required, Validators.email]);
 
-  // getErrorMessage() {
-  //   return this.email.hasError('required') ? 'You must enter a value' :
-  //       this.email.hasError('email') ? 'Not a valid email' :
-  //           '';
-  // }
-
- 
-  //     email: '';
-  //     password: '';
-  //     repeatPassword: '';
-  //     instructor: false;
-  //     msg: '';
-  //     signed: false;
-   
-  // validations: {
-  //   password: {
-  //     required,
-  //     // minLength: minLength(8)
-  //   },
-  //   repeatPassword: {
-  //     // sameAsPassword: sameAs('password')
-  //   },
-  //   instructor: {
-  //     // sameAs: sameAs(() => true)
-  //   }
-  // }
-
-  constructor(private router: Router,  private userService: UserService) { }
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
   }
 
   onRegister(data) {
-    // this.msg = ''
-    // let emailid = this.email
-    // if (data.valid) {
-      console.log(data.value);
-      // return true;
-      this.userService
-        .validateEmail(data.value)
-        .subscribe((res : any) => {
-          console.log(res)
-          if (!res.message.data) {
-            // console.log('onregister clicked fullnaem', this.email)
-            this.userService
-              .signup(data.value)
-              .subscribe((res : any) => {
-                // if (res.message.data) {
-                //   setTimeout(function () {
-                    
-                //   }, 1)
-                // }
-                // localStorage.setItem('token', res.token)
-                // localStorage.setItem('status', res.isInstructor)
-                this.router.navigate(['/login'])
-              },
-              err => {
-                console.log(err)
-                 alert("Invalid credentials");
-              })
-          } 
-          
-          else {
-            alert('Invalid Email')
-          }
-        },
-        err => {
-          console.log(err)
-          // alert("Invalid credentials");
-        })
-    }
-
-  
+    console.log(data.value);
+    this.userService
+      .validateEmail(data.value)
+      .subscribe((res: any) => {
+        console.log(res)
+        if (!res.message.data) {
+          this.userService
+            .signup(data.value)
+            .subscribe((res: any) => {
+              this.router.navigate(['/login'])
+            },
+            err => {
+              console.log(err)
+              alert("Invalid credentials");
+            })
+        }
+        else {
+          alert('Invalid Email')
+        }
+      },
+      err => {
+        console.log(err)
+      })
+  }
 
   rowClicked(row: any): void {
     console.log(row);
