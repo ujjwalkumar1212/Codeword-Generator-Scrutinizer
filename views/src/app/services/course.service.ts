7348d897d5482b353fa2844d214d6458636b953f
ANURAG KUMAR <s531372@nwmissouri.edu>
2019-02-21 20:21:31 -0600

    created API call service for coursestudentdata function

--- views/src/app/services/course.service.ts
+++ views/src/app/services/course.service.ts
@@ -9,7 +9,19 @@ export class CourseService {
     constructor(private httpclient: HttpClient) { }
 
     //API calls starts here
-    
+    getCourseStudentData(data) {
+        return this.httpclient.post("http://localhost:3000/codeword/getcoursestudent", {
+            CourseNameValue: data
+        }, {
+                headers: {
+                    token: window.localStorage.getItem('token')
+                }
+            })
+    }
 
-    
+
+
+
+
+
 }
