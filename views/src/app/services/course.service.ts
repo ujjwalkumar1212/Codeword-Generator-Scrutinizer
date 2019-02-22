b2c0d0a842f101393d6f1279804a16568105cb1a
ujjwalkumar1212 <31749848+ujjwalkumar1212@users.noreply.github.com>
2019-02-21 18:42:01 -0600

    Service created for Student Course details model

---
+++ views/src/app/services/course.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CourseService {
+
+    constructor(private httpclient: HttpClient) { }
+
+    //API calls starts here
+    
+
+    
+}
